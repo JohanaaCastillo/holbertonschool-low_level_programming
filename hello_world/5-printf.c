3f336ff48221f010371d8ed0946e0358e231bff2
JohanaaCastillo <johana.cas.code@gmail.com>
2023-06-19 16:17:44 -0700

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descripcion: "with proper grammar, but the outcome is a piece of art"
+ *
+ * Return: Always zero success
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
