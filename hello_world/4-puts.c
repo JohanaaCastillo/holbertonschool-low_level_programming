1efd9c564e752092c9d0286ed2fc13d41fdb2768
JohanaaCastillo <johana.cas.code@gmail.com>
2023-06-19 13:53:06 -0700

    prints "Programming is like building a multilingual puzzle

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main:Second program
+ *
+ * description:Write on the screen
+ *
+ * return:zero on success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
