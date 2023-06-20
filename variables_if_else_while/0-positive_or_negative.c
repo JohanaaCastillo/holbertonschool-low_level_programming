6865db73704a7c7ec0371c68a82e29997ba59414
JohanaaCastillo <johana.cas.code@gmail.com>
2023-06-20 14:22:44 -0700

    update positive negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Descripcion: "This program will assign a random number to the variable n
+ * each time it is executed"
+ *
+ * Return: Always zero success
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+	/* if condition is true then print the following */
+		printf("%d Number is positive\n", n)
+	}
+	else if (n == 0)
+	{
+		printf("%d Number is zero\n", n);
+	}
+	else
+	{
+		printf("%d Number is negative\n", n)
+	}
+	return (0);
+}
