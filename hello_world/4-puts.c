004f987cc9afc313fd972619e516c72026ab1aab
JohanaaCastillo <johana.cas.code@gmail.com>
2023-06-25 15:43:48 -0700

    c program: prints "Programming is like building a multilingual puzzle

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main: Entry point
+ *
+ * Description: "Programming is like building a multilingual puzzle"
+ *
+ * Return : Always zero success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
