12e708d9395e5b7b80fe9c18c6eaf62dc6ff990b
JohanaaCastillo <johana.cas.code@gmail.com>
2023-06-25 16:36:48 -0700

    c program : update betty documentation

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description: "Programming is like building a multilingual puzzle"
  *
- * Return : Always zero success
+ * Return: Always zero success
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"\n);
 	return (0);
 }
