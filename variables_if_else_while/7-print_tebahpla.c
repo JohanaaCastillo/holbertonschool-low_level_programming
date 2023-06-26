6122f4a2d382a306eee917a0d6845c910dd9825b
JohanaaCastillo <johana.cas.code@gmail.com>
2023-06-26 13:31:18 -0700

    c program: prints the lowercase alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+ *
+ * Descripcion: "Write a program  that prints the
+ * lowercase alphabet in reverse"
+ *
+ * Return: Always zero success
+*/
+
+int main(void)
+{
+	int alphabet = 122;
+		while (alphabet < 97)
+		{
+			putchar(alphabet);
+		}
+			putchar('\n');
+			return (0);
+}
