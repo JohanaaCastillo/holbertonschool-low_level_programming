23eb9d706a39567ce8f803db44e5c2b6c1c8e6ef
JohanaaCastillo <johana.cas.code@gmail.com>
2023-07-09 15:54:24 -0700

    function:update

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "6-abs.c"
 /**
  * print_last_digit - Write a function the last digit of a number
  *
@@ -10,13 +10,6 @@
 
 int print_last_digit(int n)
 {
-	int last;
-
-	last = n % 10;
-	if (last < 10)
-	{
-		last = last * -1;
-	}
-		_putchar(last + '0');
-		return (last);
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
 }
