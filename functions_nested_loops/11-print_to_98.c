e2e799d10c26b59809cbd4f88f8a0b27fe34443e
JohanaaCastillo <johana.cas.code@gmail.com>
2023-07-09 17:11:55 -0700

    update

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "6-abs.c"
 #include <stdio.h>
 /**
- * add - prints all natural numbers from n to 98
+ * print_to_98 - prints all natural numbers from n to 98
  *
  * @n : the number to start printing from
  *
