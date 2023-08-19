8c6224c5da8f26f990d590586428a1445f68cd94
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-19 17:39:17 -0500

    Adding files

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+* print_name - Function that prints a name
+*
+*@name: pointer
+*@f: pointer of function
+*
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
