e2e20fa48e0fa2366c34551e73c793a1d0ce2ecb
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-19 12:54:24 -0500

    Add psitive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - Add positive numbers
+ *
+ * @argc: numbers
+ * @argv: arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i, j, Sum;
+
+	Sum = 0;
+	if (argc == 1)
+		printf("0\n");
+	if (argc > 1)
+		{
+		for (i = 1; i < argc; i++)
+		{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+				Sum = Sum + atoi(argv[i]);
+		}
+			printf("%d\n", Sum);
+		}
+	return (0);
+}
