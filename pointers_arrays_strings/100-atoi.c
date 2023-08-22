13ad10aa0d7a0abb8c9d763af5ddc609352ffbae
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-22 17:32:25 -0500

    Fix error

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,64 +1,35 @@
 #include "main.h"
 
 /**
-* _strlen - Function that returns the lenght of a string
-*
-* @s: string
-*
-* Return: Always 0.
-*/
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-
-	return (i);
-}
-
-/**
  * _atoi - Function that convert a string to an integer
  *
  * @s: string to convert
  *
  * Return: return integer value.
  */
+
 int _atoi(char *s)
 {
-int i;
-int np = 0;
-int c;
-int d = 1;
-int num = 0;
+int i = 0, j, cm = 0, digit = 0;
+unsigned int n = 0;
 
-for (i = 0; i < _strlen(s); i++)
+while (s[i])
 {
-if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
+i++;
+}
+for (j = 0; j < i; j++)
+{
+if (s[j] == '-')
+cm++;
+if (s[j] >= '0' && s[j] <= '9')
+{
+digit = s[j] - '0';
+n = n * 10 + digit;
+if (s[j + 1] < '0' || s[j + 1] > '9')
 break;
-if (s[i] == '-')
-np--;
-if (s[i] == '+')
-np++;
-if (s[i] >= '0' && s[i] <= '9')
-{
-c++;
 }
 }
-while (c > 0)
-{
-num += ((s[i - 1] - '0') * d);
-i--;
-c--;
-d *= 10;
-}
-if (np >= 0)
-{
-num *= 1;
-}
-else
-{
-num *= -1;
-}
-return (num);
+if (cm % 2 == 1)
+n = n * (-1);
+return (n);
 }
