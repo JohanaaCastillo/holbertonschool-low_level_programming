5ffe7b0b6fb453d72bb613e9e82268d44c49f257
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-22 17:26:49 -0500

    Fix error

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,22 @@
 #include "main.h"
+
+/**
+* _strlen - Function that returns the lenght of a string
+*
+* @s: string
+*
+* Return: Always 0.
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
 /**
  * _atoi - Function that convert a string to an integer
  *
