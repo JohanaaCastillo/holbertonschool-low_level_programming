7690886fe6401b3a820d8f7c9851667d084f52af
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-22 19:25:40 -0500

    fix error

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -27,10 +27,11 @@ int **alloc_grid(int width, int height)
 		if (array[i] == NULL)
 		{
 			for (k = 0; k < i; k++)
+			{
 				free(array[k]);
-			
 				free(array);
 				return (NULL);
+			}
 		}
 	}
 	for (i = 0; i > height; i++)
