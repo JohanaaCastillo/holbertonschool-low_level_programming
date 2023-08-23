f5440f78b5a057c42dfa0f417f709ec7609827f3
JohanaaCastillo <johana.cas.code@gmail.com>
2023-08-23 08:41:53 -0500

    Fix betty style error

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,98 +2,89 @@
 #include <stdio.h>
 
 /**
- * print_char - Function that prints anything
- * @a: list to give
+ * print_char - function that prints chars
+ * @separator: char separator
+ * @ap: arg variaditic value
+ */
+
+void print_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+ * print_integer - function that prints integers
+ * @separator: char separator
+ * @ap: arg variadict value
+ */
+
+void print_integer(char *separator, va_list ap)
+{
+	printf("%s%d", separator, va_arg(ap, int));
+}
+
+/**
+ * print_float - function that prints floats
+ * @separator: char separator
+ * @ap: arg variadict value
+ */
+
+void print_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_arg(ap, double));
+}
+
+/**
+ * print_string - function that prints strings
+ * @separator: char separator
+ * @ap: arg caraidict value
+ */
+
+void print_string(char *separator, va_list ap)
+{
+	char *str = va_arg(ap, char *);
+
+	if ((int)(!str))
+		str = "(nil)";
+	printf("%s%s", separator, str);
+}
+
+/**
+ * print_all - function that prints anything
  *
- */
-
-int print_c(va_list a)
-{
-	printf("%c", va_arg(a, int));
-	return (0);
-}
-
-/**
- * print_i - prints int
- * @a: list to give
- * Return: always 0
- */
-
-int print_i(va_list a)
-{
-	printf("%d", va_arg(a, int));
-	return (0);
-}
-
-/**
- * print_f - prints float
- * @a: list to give
- * Return: always 0
- */
-
-int print_f(va_list a)
-{
-	printf("%f", va_arg(a, double));
-	return (0);
-}
-
-/**
- * print_s - prints string
- * @a: list to give
- * Return: always 0
- */
-
-int print_s(va_list a)
-{
-	char *s;
-
-	s = va_arg(a, char *);
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return (0);
-	}
-	printf("%s", s);
-	return (0);
-}
-
-/**
- * print_all - prints all
- * @format: format string that says arg types
+ * @format: const pointer to char to cons
  *
+ * Return: the function to be used depening of format
  */
 
 void print_all(const char * const format, ...)
 {
-	int i, j;
-	char *sep = "";
-	char *sep2 = ", ";
-	va_list anyArgs;
-	printer ops[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"s", print_s},
-		{"f", print_f},
-		{NULL, NULL}
+	fmt frm[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL},
 	};
+	int i = 0, j;
+	char *separator = "";
+	va_list ap;
 
-	va_start(anyArgs, format);
-	i = 0;
-	while (format != NULL && format[i])
+	va_start(ap, format);
+	while (format && format[i])
 	{
 		j = 0;
-		while (ops[j].f != NULL)
+		while (frm[j].format)
 		{
-			if (format[i] == *(ops[j].c))
+			if (format[i] == frm[j].format[0])
 			{
-				printf("%s", sep);
-				ops[j].f(anyArgs);
+				frm[j].f(separator, ap);
+				separator = ", ";
 			}
 			j++;
 		}
-		sep = sep2;
 		i++;
 	}
 	printf("\n");
-	va_end(anyArgs);
+	va_end(ap);
 }
